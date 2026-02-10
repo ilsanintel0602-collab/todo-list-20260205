9f7204a6e3d0511d20e39b34b2c60c7f8f43ca2f
ilsanintel0602-collab <ilsanintel0602@gmail.com>
2026-02-10 13:54:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const path = require('path');
 const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 const session = require("express-session");
@@ -22,6 +23,19 @@ app.use(session({
 // ✅ 그 다음 passport
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => done(null, profile)
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 // 기타 미들웨어
 app.use(cors());
